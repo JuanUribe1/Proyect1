8c9ba26b317aaf5b98bb344baaebe2e772efe9bd
Juan Pablo Uribe <juan.uribe@techandsolve.com>
2022-04-19 14:15:58 -0500

    mejora salida consola

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,5 +1,18 @@
+package Main;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("too easy");
+        Persona estudiante = new Persona();
+        estudiante.setNombre(JOptionPane.showInputDialog("Ingrese su nombre"));
+        estudiante.setApellidos(JOptionPane.showInputDialog("Ingrese su apellido"));
+        estudiante.setID(JOptionPane.showInputDialog("Ingrese su ID"));
+        estudiante.setCorreo(JOptionPane.showInputDialog("Ingrese su correo electronico"));
+        estudiante.setPais(JOptionPane.showInputDialog("Ingrese su pais de residencia"));
+        estudiante.setEdad(JOptionPane.showInputDialog("Ingrese su Edad"));
+
+        System.out.println(estudiante.getNombre()+" "+estudiante.getApellidos()+" "+ estudiante.getID()+" "+estudiante.getCorreo()+" "+estudiante.getPais()+" "+estudiante.getEdad());
     }
 }
--- src/Main/Persona.java
+++ src/Main/Persona.java
@@ -1,2 +1,65 @@
-package Main;public class Persona {
+package Main;
+
+public class Persona {
+    private String Nombre, Apellidos, ID, Edad, Correo, Pais;
+
+    public Persona() {
+    }
+
+    public Persona(String Nombre, String Apellidos, String ID, String Edad, String Correo, String Pais) {
+        this.Nombre = Nombre;
+        this.Apellidos = Apellidos;
+        this.ID = ID;
+        this.Edad = Edad;
+        this.Correo = Correo;
+        this.Pais = Pais;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return Apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        Apellidos = apellidos;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getEdad() {
+        return Edad;
+    }
+
+    public void setEdad(String edad) {
+        Edad = edad;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String correo) {
+        Correo = correo;
+    }
+
+    public String getPais() {
+        return Pais;
+    }
+
+    public void setPais(String pais) {
+        Pais = pais;
+    }
 }
